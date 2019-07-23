46dfc25fc96f34dd4d2e04828c2475b0a6f1969c
Mrudula <mrudula@ivicatechnologies.com>
2019-07-23 16:01:39 +0530

    Commit - Navigation drawer with navigation

---
+++ app/src/main/java/com/example/navigation/FirstFragment.java
@@ -0,0 +1,33 @@
+package com.example.navigation;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+public class FirstFragment extends Fragment {
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.btnNavigate).setOnClickListener
+                (Navigation.createNavigateOnClickListener(R.id.next_action));
+    }
+}
---
+++ app/src/main/java/com/example/navigation/ThirdFragment.java
@@ -0,0 +1,38 @@
+package com.example.navigation;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ThirdFragment extends Fragment {
+
+    public ThirdFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.btnNavigate).setOnClickListener
+                (Navigation.createNavigateOnClickListener(R.id.next_action));
+    }
+}
